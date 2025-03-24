ac4b84d81dd05417e9205119964cff8335500811
Anirban Sarkar <137922245+Anirban780@users.noreply.github.com>
2025-03-24 15:54:35 +0530

    updated server and api js # 3

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,8 @@ app.use(cors({
         }
     },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS' ]
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS' ],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 
