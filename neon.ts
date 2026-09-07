2b4aae2a0c5455b7501b1fcb9c4c15b6212fada2
Anirban780 <amalanir7@gmail.com>
2026-09-07 16:23:43 +0530

    docs(deployment): add deployment plans and updated env configuration

---
+++ neon.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "@neon/config/v1";
+
+/**
+ * Neon Configuration as Code
+ * Project: restless-bread-66524216
+ * Branch: production
+ */
+export default defineConfig({});
